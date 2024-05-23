bb48ccc2d1d931c9f0ba2f97dc6c6b58ac7f85e1
SanSpeen <ktns0930@gmail.com>
2024-05-23 02:41:24 -0500

    added exclusions tag

--- src/main/java/com/app/todo/be/DTO/TaskDTO.java
+++ src/main/java/com/app/todo/be/DTO/TaskDTO.java
@@ -1,12 +1,15 @@
 package com.app.todo.be.DTO;
 
 
+import com.app.todo.be.config.JacocoExcludeAnnotationGenerated;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
+
+@JacocoExcludeAnnotationGenerated
 @Getter
 @Setter
 @AllArgsConstructor
